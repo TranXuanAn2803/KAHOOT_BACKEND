b28c94ced315d6d71e8210da6ea37fd837667a52
tuan-khanh <nguyentuankhanhcqt@gmail.com>
2022-12-14 11:47:51 +0700

    cho phép slide có câu hỏi mặc định khi ko cung cấp question

--- src/slide/slide.model.js
+++ src/slide/slide.model.js
@@ -1,36 +1,38 @@
 const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 const Slide = mongoose.model(
-    "slide",
-    new Schema(
+  "slide",
+  new Schema(
+    {
+      question: {
+        type: String,
+        default: "MULTIPLE_CHOICE",
+      },
+      // type: {
+      //     type: Schema.Types.Number,
+      //     required: true,
+      // },
+      slide_type: {
+        type: String,
+        default: "MULTIPLE_CHOICE",
+      },
+      presentation_id: {
+        type: Schema.Types.ObjectId,
+        ref: "presentations",
+      },
+      index: {
+        type: Number,
+        required: true,
+      },
+      answer: [
         {
-        question: {
-            type: String,
-            required: true,
+          type: Schema.Types.ObjectId,
+          ref: "options",
+          default: null,
         },
-        // type: {
-        //     type: Schema.Types.Number,
-        //     required: true,
-        // },
-        slide_type: {
-            type: String,
-            default: 'MULTIPLE_CHOICE',
-        },
-        presentation_id:{
-            type: Schema.Types.ObjectId, 
-            ref: 'presentations' 
-        },
-        index:{
-            type: Number,
-            required: true
-        },
-        answer: [{ 
-            type: Schema.Types.ObjectId, 
-            ref: 'options', 
-            default: null
-        }],
-        },
-        { timestamps: true }
-    )
+      ],
+    },
+    { timestamps: true }
+  )
 );
 module.exports = Slide;
