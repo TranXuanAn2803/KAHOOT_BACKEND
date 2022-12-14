ca28603a55242d057da54d5f70fcabbde62d82f7
TranXuanAn2803 <tranxuana2803@gmail.com>
2022-12-14 15:28:53 +0700

    create method delete

--- src/slide/option/option.method.js
+++ src/slide/option/option.method.js
@@ -4,12 +4,13 @@ const Slide = require("../slide.model");
 
 const { Types } = require("mongoose");
 const { response } = require("express");
+const { User } = require("../../user/user.model");
 
 const addOptionsBySlide = async (slideId, options) => {
     const prevOptions = await Option.find({slide_id: slideId});
     try {
         if (prevOptions&&prevOptions.length> 0) {
-            prevOptions.map(async(p)=>{
+            await prevOptions.map(async(p)=>{
                 await Option.deleteOne({ _id: p._id });  
             })
         }
@@ -83,9 +84,29 @@ const getTotalAnswerBySlide =async(slideId)=>{
     }
     
 }
+const deleteBySlide = async (id) => {
+    try {
+        const slide = await Slide.findOne({_id: id});
+        if (!slide) 
+            return false;
+        const options = await Option.find({slide_id: id});
+        if (options&&options.length> 0) {
+            options.map(async(o)=>{
+                await UserOption.deleteMany({ option_id: o.id });
+            })
+        }
+        return true;
+    }
+    catch(err){
+        console.error(err)
+        return false;
+    }
+
+};
 
 module.exports = {
     addOptionsBySlide,
     addUserAnswer,
-    getTotalAnswerBySlide
+    getTotalAnswerBySlide,
+    deleteBySlide
 }
