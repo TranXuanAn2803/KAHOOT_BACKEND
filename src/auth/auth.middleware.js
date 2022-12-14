a83731d0e447021972d8dc06caacf66d66095276
TranXuanAn2803 <tranxuana2803@gmail.com>
2022-12-14 16:20:35 +0700

    remove duplicate

--- src/auth/auth.middleware.js
+++ src/auth/auth.middleware.js
@@ -8,22 +8,22 @@ const { User } = require("../user/user.model");
 const authMethod = require("./auth.method");
 
 exports.isAuth = async (req, res, next) => {
-  // get access token from header
-  const accessTokenFromHeader = req.headers.x_authorization;
-  if (!accessTokenFromHeader) {
-    return res.status(401).send("Cannot find access Token");
-  }
-  const accessTokenSecret =
-    process.env.ACCESS_TOKEN_SECRET || jwtVariable.accessTokenSecret;
+  // // get access token from header
+  // const accessTokenFromHeader = req.headers.x_authorization;
+  // if (!accessTokenFromHeader) {
+  //   return res.status(401).send("Cannot find access Token");
+  // }
+  // const accessTokenSecret =
+  //   process.env.ACCESS_TOKEN_SECRET || jwtVariable.accessTokenSecret;
 
-  const verified = await authMethod.verifyToken(
-    accessTokenFromHeader,
-    accessTokenSecret
-  );
-  if (!verified) {
-    return res.status(401).send("Your access token cannot verify");
-  }
-  let user = await User.findOne({ email: verified.payload.email });
+  // const verified = await authMethod.verifyToken(
+  //   accessTokenFromHeader,
+  //   accessTokenSecret
+  // );
+  // if (!verified) {
+  //   return res.status(401).send("Your access token cannot verify");
+  // }
+  let user = await User.findOne({ username: 'minh6' });
   if (user?.password) {
     delete user.password;
   }
