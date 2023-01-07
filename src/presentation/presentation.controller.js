cfbdf14cf88c827ce5666f9c2525c3ce67f8c861
TranXuanAn2803 <tranxuana2803@gmail.com>
2023-01-07 22:55:17 +0700

    sharing present

--- src/presentation/presentation.controller.js
+++ src/presentation/presentation.controller.js
@@ -43,28 +43,6 @@ const getMyOwnPrensent = async (req, res) => {
     )
       .sort({ createdAt: -1 })
       .populate({
-        path: "created_by",
-        model: User,
-        select: "username email",
-      })
-      .lean();
-    return res.status(200).send({ data: present });
-  } catch (err) {
-    console.error(err);
-    return res.status(400).send({ message: 'Error in database conection' });
-  }
-};
-const getMyOwnPrensent = async (req, res) => {
-  const user = req.user;
-  if (!user) {
-    return res.status(400).send('User not found');
-  }
-  try {
-    const present = await Presentation.find(
-      { created_by: user._id },
-    )
-      .sort({ createdAt: -1 })
-      .populate({
         path: 'created_by',
         model: User,
         select: 'username email',
