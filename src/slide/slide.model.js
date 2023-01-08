7e892662e19dbe004bed25cdaeae6f10cc1a4d21
tuan-khanh <nguyentuankhanhcqt@gmail.com>
2023-01-08 17:24:37 +0700

    thêm type của slide (tham chiếu như enum).

--- src/slide/slide.model.js
+++ src/slide/slide.model.js
@@ -1,38 +1,45 @@
 const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 const Slide = mongoose.model(
-    "slide",
-    new Schema(
+  "slide",
+  new Schema(
+    {
+      question: {
+        type: String,
+        default: "MULTIPLE_CHOICE",
+      },
+      type: {
+        type: Schema.Types.Number,
+        // required: true,
+      },
+      slide_type: {
+        type: String,
+        default: "MULTIPLE_CHOICE",
+      },
+      presentation_id: {
+        type: Schema.Types.ObjectId,
+        ref: "presentations",
+      },
+      index: {
+        type: Number,
+        required: true,
+      },
+      answer: [
         {
-        question: {
-            type: String,
-            default: "MULTIPLE_CHOICE",
+          type: Schema.Types.ObjectId,
+          ref: "options",
+          default: null,
         },
-        // type: {
-        //     type: Schema.Types.Number,
-        //     required: true,
-        // },
-        slide_type: {
-            type: String,
-            default: "MULTIPLE_CHOICE",
-        },
-        presentation_id: {
-            type: Schema.Types.ObjectId,
-            ref: "presentations",
-        },
-        index: {
-            type: Number,
-            required: true,
-        },
-        answer: [
-            {
-            type: Schema.Types.ObjectId,
-            ref: "options",
-            default: null,
-            },
-        ],
-        },
-        { timestamps: true }
-    )
+      ],
+    },
+    { timestamps: true }
+  )
 );
-module.exports = Slide;
+
+const SlideType = {
+  MultipleChoice: 1,
+  Heading: 2,
+  Paragraph: 3,
+};
+
+module.exports = { Slide, SlideType };
